ac86f8bd187150aa44c3a58adf88a45ec682b2e8
Albert Martin <albertmartin@mac.com>
2013-11-19 02:42:55 +0000

    Updated API to point to new fields.

--- modules/bethel_api/lib/Drupal/bethel_api/BethelAPIController.php
+++ modules/bethel_api/lib/Drupal/bethel_api/BethelAPIController.php
@@ -13,7 +13,7 @@ use Drupal\Component\Utility\Json;
 
 class BethelAPIController {
   public function locations() { 
-    $locations = db_query("SELECT l.field_locations_location AS 'address', l.field_locations_locationLatitude AS 'lat', l.field_locations_locationLongitude AS 'long', u.uid FROM {user__field_locations} l LEFT JOIN {users} u ON l.entity_id = u.uid")->fetchAllAssoc('address');
+    $locations = db_query("SELECT l.field_locations_location AS 'address', l.field_locations_locationLatitude AS 'lat', l.field_locations_locationLongitude AS 'long', u.uid FROM {node__field_locations} l LEFT JOIN {users} u ON l.entity_id = u.uid")->fetchAllAssoc('address');
     // Return a JSON formatted array.
     return new JsonResponse($locations);
   }
