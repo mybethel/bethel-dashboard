0a29a6bc401d354874175e12d276f6477fec5697
Albert Martin <albertmartin@mac.com>
2014-01-17 01:47:28 +0000

    Composer-modified autoloaders.

--- core/vendor/autoload.php
+++ core/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit50432d86c534be2ddddf85d27e021721::getLoader();
+return ComposerAutoloaderInit43fb9a41ba4ac691859d635c41653d16::getLoader();
